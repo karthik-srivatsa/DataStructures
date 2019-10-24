e2924d9f447a6d165d29556055d7a798b7a9bd28
Karthik <Karth@LAPTOP-HCM2I4A0>
2019-10-24 22:16:24 +0530

    Added few methods of Linked List

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+
+namespace LinkedList
+{
+    public class LinkedList<T> where T : IComparable
+    {
+        private Node<T> _head;
+        private Node<T> _tail;
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// adding element at the beginning of the List
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddFirst(T item)
+        {
+            var node = new Node<T> { Item = item };
+
+            node.Next = _head;
+            _head = node;
+
+            if (_tail == null)
+                _tail = node;
+                
+            Count++;
+        }
+
+        /// <summary>
+        /// Adds the item in last
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddLast(T item)
+        {
+            var node = new Node<T> { Item = item };
+            
+            if (_head == null)
+                _head = node;
+            else
+                _tail.Next = node;
+            
+            _tail = node;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(T item)
+        {
+            return IndexOf(item) >= 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public int IndexOf(T item)
+        {
+            int index = 0;
+            if (_head == null)
+                return -1;
+
+            var temp = _head;
+            do
+            {
+                if (item.CompareTo(temp.Item) == 0)
+                    return index;
+                index++;
+                temp = temp.Next;
+            } while (temp.Next != null);
+
+            return -1;
+        }
+    }
+}
---
+++ LinkedList/Node.cs
@@ -0,0 +1,9 @@
+﻿
+namespace LinkedList
+{
+    internal class Node<T>
+    {
+        internal T Item { get; set; }
+        internal Node<T> Next { get; set; }
+    }
+}
